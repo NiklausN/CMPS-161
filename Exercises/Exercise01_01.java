c1018a1d5785e2aaebfe9e9da068c4b657e53ced
Niklaus <60277645+NiklausN@users.noreply.github.com>
2020-06-04 17:55:36 -0500

    Update Exercise01_01.java

--- Exercises/Exercise01_01.java
+++ Exercises/Exercise01_01.java
@@ -1,1 +1,12 @@
+public class Exercise01_01 {
 
+    public static void main(String[] args) {
+
+        // Print a Few Statements
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Computer Science!");
+        System.out.print("Programming is fun!");
+
+    } // End of Main Method
+
+} // End of Exercise01_01 Class
